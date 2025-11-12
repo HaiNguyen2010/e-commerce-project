4a6e05abecd6b72fb8766820c90f89f3e6f420a4
Finn <thantrym@gmail.com>
2025-11-12 22:17:28 +0700

    Code function filtering

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,14 +2,51 @@ const Product = require("../../models/product.model.js");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        deleted: false
-    });
+    let filterStatus = [
+        {
+            name: "All",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Active",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Inactive",
+            status: "inactive",
+            class: ""
+        }
+    ]
 
-    console.log(products);
+    let find = {
+        deleted: false
+    };
+
+    if(req.query.status) {
+        const index = filterStatus.findIndex((item) => {
+            return item.status == req.query.status;
+        })
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex((item) => {
+            return item.status == "";
+        });
+        filterStatus[index].class = "active";
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status;
+        
+    }
+    
+    const products = await Product.find(find);
+    // console.log(products);
 
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Product Management",
-        products: products
+        products: products,
+        filterStatus: filterStatus,
     });
 }
