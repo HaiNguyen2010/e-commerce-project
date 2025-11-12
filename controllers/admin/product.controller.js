4459933d66e2d2e89cc58ef7396765ed198ed58b
Finn <thantrym@gmail.com>
2025-11-12 21:26:31 +0700

    Get product data and render

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,15 @@
+const Product = require("../../models/product.model.js");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products);
+
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Product Management"
+        pageTitle: "Product Management",
+        products: products
     });
 }
