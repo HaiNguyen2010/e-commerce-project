08f492e9a785baa168bb340340d95fa3330abcb3
Finn <thantrym@gmail.com>
2025-11-12 20:55:55 +0700

    Build admin layout

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ const routeAdmin = require("./routes/admin/index.route.js");
 require("dotenv").config();
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
 // Connect to database
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
+// View engine setup
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+
+// App Locals Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 // Routes
 route(app);
