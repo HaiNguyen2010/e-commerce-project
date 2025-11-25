6dab9788de92ed6805c2329333fa54f5e30fe13f
Finn <thantrym@gmail.com>
2025-11-25 22:15:47 +0700

    Build Change Item Position Function

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -33,7 +33,11 @@ module.exports.index = async (req, res) => {
     );
     // End Pagination
 
-    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
+    const products = await Product        
+        .find(find)
+        .sort({ position: "desc" })
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
     // console.log(products);
 
     res.render("admin/pages/products/index.pug", {
@@ -74,6 +78,14 @@ module.exports.changeMulti = async (req, res) => {
                 deleted: true,
                 deletedAt: new Date()
             });
+            break;
+        case "change-position":
+            for (const item of ids) {
+                let [id, position] = item.split("-");
+                position = parseInt(position);
+                await Product.updateOne({ _id: id }, { position: position });
+            }
+            break;
         default:
             break;
     }
