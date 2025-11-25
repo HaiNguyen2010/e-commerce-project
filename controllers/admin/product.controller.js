cf54668474931ac4d17b1ebc7f5630b97b5aa35c
Finn <thantrym@gmail.com>
2025-11-25 21:13:58 +0700

    Update Delete Function: Add Deleted Time

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -80,7 +80,10 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
     const id = req.params.id;
 
-    await Product.updateOne({ _id: id }, { deleted: true });
+    await Product.updateOne({ _id: id }, { 
+        deleted: true,
+        deletedAt: new Date()
+    });
 
     res.redirect(req.get("referer") || "/products");
 }
